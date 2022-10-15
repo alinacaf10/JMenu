e8f77be68f2705fd46bf2119c4ee28862466a5d6
FARHAD  NAJAFOV <farhadnajafov98.com>
2022-10-15 12:24:52 +0400

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        JMenuBar mb=new JMenuBar();
+
+        frame.setJMenuBar(mb);
+        frame.setSize(400,400);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
