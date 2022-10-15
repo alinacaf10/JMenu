216225a6f81017d7437e7c329e11a92938a1d2fd
FARHAD  NAJAFOV <farhadnajafov98.com>
2022-10-15 12:29:53 +0400

    Menubar

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,10 +7,23 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame=new JFrame();
         JMenuBar mb=new JMenuBar();
+        JMenu menu=new JMenu("Main");
 
-        frame.setJMenuBar(mb);
+        JMenuItem m1=new JMenuItem("Menu one");
+        JMenuItem m2=new JMenuItem("Menu two");
+        JMenuItem m3=new JMenuItem("Menu three");
+        JMenuItem m4=new JMenuItem("Menu four");
+
+        menu.add(m1);
+        menu.add(m2);
+        menu.add(m3);
+        menu.add(m4);
+
         frame.setSize(400,400);
         frame.setLayout(null);
         frame.setVisible(true);
+        mb.add(menu);
+        frame.setJMenuBar(mb);
+
     }
 }
